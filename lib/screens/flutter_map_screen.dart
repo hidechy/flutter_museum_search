a86e3714df7fb879a3840df9aa79dac8476aa728
toyohide <hide.toyoda@gmail.com>
2023-04-26 14:26:03 +0900

    commit 20230426

--- lib/screens/flutter_map_screen.dart
+++ lib/screens/flutter_map_screen.dart
@@ -3,6 +3,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 //import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
