aca463d734edbff1ede12eaefdcfed06b5f9079f
toyohide <hide.toyoda@gmail.com>
2023-05-09 08:38:34 +0900

    commit 20230509 search complete

--- lib/screens/station_name_search_screen.dart
+++ lib/screens/station_name_search_screen.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../models/train_station.dart';
 
-class StationNameSearchScreen extends ConsumerWidget {
+class StationNameSearchScreen extends StatelessWidget {
   const StationNameSearchScreen({super.key, required this.trainStationList});
 
   final List<Station> trainStationList;
@@ -17,57 +16,72 @@ class StationNameSearchScreen extends ConsumerWidget {
 
   ///
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 20),
+            /////
+
             Autocomplete<Station>(
-                displayStringForOption: _displayStringForOption,
-                optionsBuilder: (TextEditingValue textEditingValue) {
-                  if (textEditingValue.text == '') {
-                    return const Iterable<Station>.empty();
-                  }
+              //
 
-                  return trainStationList.where((Station station) {
-                    return station.stationName.contains(textEditingValue.text);
-                  });
-                },
-                optionsViewBuilder: (context, onSelected, trainStationList) {
-                  return ListView.builder(
-                      itemCount: trainStationList.length,
-                      itemBuilder: (context, index) {
-                        final stationList = trainStationList.toList();
+              displayStringForOption: _displayStringForOption,
 
-                        return StationNameCard(stationList[index]);
-                      });
-                }),
+              //
+
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text == '') {
+                  return const Iterable<Station>.empty();
+                }
+
+                return trainStationList.where((Station station) {
+                  return station.stationName.contains(textEditingValue.text);
+                });
+              },
+
+              //
+
+              optionsViewBuilder: (context, onSelected, trainStationList) {
+                return ListView.builder(
+                  itemCount: trainStationList.length,
+                  itemBuilder: (context, index) {
+                    final stationList = trainStationList.toList();
+
+                    return Card(
+                      child: ListTile(
+                        title: Text(
+                          stationList[index].stationName,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        subtitle: Text(
+                          stationList[index].lineName,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        trailing: GestureDetector(
+                          onTap: () {
+                            print(stationList[index].stationName);
+
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            Icons.input,
+                            color: Colors.white.withOpacity(0.6),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+
+              //
+            ),
+
+            /////
           ],
         ),
-      ),
-    );
-  }
-}
-
-class StationNameCard extends StatelessWidget {
-  StationNameCard(this.station, {super.key});
-
-  Station station;
-
-  ///
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(station.stationName),
-        subtitle: Text(station.lineName),
-        onTap: () {
-          print(station.stationName);
-
-          Navigator.pop(context);
-        },
       ),
     );
   }
